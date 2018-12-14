be903ab628c161ff7fa42b500aee1fe609e85686
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-14 22:35:11 +0900

    Update index.js
    add Wallet & Chain

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const sha256 = require('./sha256.js');
 const coord = require('./coord.js');
 const Block = require('./block.js');
+const Chain = require('./chain.js');
+const Wallet = require('./wallet.js');
 const util = require('./util.js');
 
 function mineGenesis(trx) {
-	return mineWithData(1, 1, util.toHex(0,64), 5, trx);
+	return mineWithData(1, 1, util.toHex(0,64), 4, trx);
 };
 
 function mineWithData(index, version, prev_hash, difficulty, trx=[]) {
@@ -26,11 +28,20 @@ function mineWithBlock(block, trx=[]) {
 	return mineWithData(block.index+1, block.version, block.hash, block.difficulty, trx);
 };
 
-let blocks = [mineGenesis(["trx : 1", "trx : 2"])];
-console.log(blocks[0]);
+let chain = new Chain();
 
-blocks.push( mineWithBlock(blocks[0], ["trx : 3", "trx : 4"]) );
-console.log(blocks[1]);
+let block = mineGenesis(["block the genesis","trx : 1", "trx : 2","padding"]);
+console.log( chain.newBlock(block), block );
 
-blocks.push( mineWithBlock(blocks[1], ["trx : 5", "trx : 6"]) );
-console.log(blocks[2]);
+let blockA = mineWithBlock(block, ["block A","trx : 3", "trx : 4","padding"]);
+console.log( chain.newBlock(blockA), blockA );
+
+let blockB = mineWithBlock(block, ["block B","trx : 5", "trx : 6","padding"]);
+console.log( chain.newBlock(blockB), blockB );
+
+let blockC = mineWithBlock(chain.topBlock, ["block C","trx : 7", "trx : 8","padding"]);
+console.log( chain.newBlock(blockC), blockC );
+
+console.log( chain.blocks );
+console.log( chain.blocks[0].hash );
+console.log( coord(chain.blocks[0].hash) );
