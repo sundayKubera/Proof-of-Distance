cf308cf1e56bb91902df6136ac7e361e4b4888c7
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-14 16:08:46 +0900

    mining code update

--- index.js
+++ index.js
@@ -3,23 +3,34 @@ const coord = require('./coord.js');
 const Block = require('./block.js');
 const util = require('./util.js');
 
+function mineGenesis(trx) {
+	return mineWithData(1, 1, util.toHex(0,64), 6, trx);
+};
 
-let genesisMiner = new Block.Miner(1, 1, util.toHex(0,64), Math.floor(Date.now()/1000), 7);
+function mineWithData(index, version, prev_hash, difficulty, trx=[]) {
+	let miner = new Block.MineHelper(index, version, prev_hash, Math.floor(Date.now()/1000), difficulty, trx);
 
-
-let startTime = Math.floor(Date.now()/1000);
-for (let i=0; true; i++) {
-	try {
-		let block = genesisMiner.mine(i);
-		if (block) {
-			console.log(block);
-			break;
+	for (let i=0; true; i++) {
+		try {
+			let block = miner.mine(i);
+			if (block)
+				return block;
+		} catch (e) {
+			return false;
 		}
-	} catch (e) {
-		console.log(e, i);
-		break;
 	}
-}
-let endTime = Math.floor(Date.now()/1000);
+	return false;
+};
 
-console.log(startTime, endTime, (endTime-startTime));
+function mineWithBlock(block, trx=[]) {
+	return mineWithData(block.index+1, block.version, block.hash, block.difficulty, trx);
+}
+
+let blocks = [mineGenesis(["trx : 1", "trx : 2"])];
+console.log(blocks[0]);
+
+blocks.push( mine(blocks[0], ["trx : 3", "trx : 4"]) );
+console.log(blocks[1]);
+
+blocks.push( mine(blocks[1], ["trx : 5", "trx : 6"]) );
+console.log(blocks[2]);
