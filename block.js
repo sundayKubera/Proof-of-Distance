c1001e2b3e2d3efe4671c73f264f6d6965bd8a82
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-15 11:39:34 +0900

    from util.js to block.js migration & encode to JSON

--- block.js
+++ block.js
@@ -1,9 +1,8 @@
-const util = require('./util.js'),
-	toHex = util.toHex;
+const util = require('./util.js');
 
 class Block {
 
-	constructor (index, version, prev_hash, mrkl_hash, timestamp, difficulty, txs, nonce, hash) {
+	constructor (hash, index, version, prev_hash, mrkl_hash, timestamp, difficulty, txsLength, txsSize, nonce, txs=false) {
 
 		this.hash = hash;
 
@@ -16,45 +15,118 @@ class Block {
 		this.timestamp = timestamp;
 		this.difficulty = difficulty;
 
-		this.txsLength = txs.length;
-		this.txsSize = JSON.stringify(txs).length;
+		this.txsLength = txsLength;
+		this.txsSize = txsSize;
 
 		this.nonce = nonce;
 
-		this.txs = txs;
-	}
+		this.txs = [];
 
-	toArray () {
-		return [
-			toHex(this.index),
-			toHex(this.version),
-
-			this.prev_hash,
-			this.mrkl_hash,
-
-			toHex(this.timestamp),
-			toHex(this.difficulty),
-
-			toHex(this.txsLength),
-			toHex(this.txsSize),
-			
-			toHex(this.nonce)
-		];
-	}
-};
-
-	Block.MineHelper = class BlockMineHelper extends Block {
-
-		constructor (index, version, prev_hash, timestamp, difficulty, txs=[]) {
-			super(index, version, prev_hash, util.calcMrklHash(txs), timestamp, difficulty, txs, 0, "");
+		if (txs !== false) {
+			if (!this.setTransactions(txs))
+				throw new Error(`Block : unValidBlock : 'transactions' or 'mrkl hash' is not valid`);
 		}
 
-		mine (nonce) {
-			this.nonce = nonce;
+		if (!Block.isBlockHeadValid(this)) {
+			throw new Error(`Block : unValidBlock : 'block head' is not valid`);
+		}
+	}
 
-			let hash = util.calcBlockHash(this), t = this;
-			if (util.isBlockHashValid(hash, this.difficulty))
-				return new Block(t.index, t.version, t.prev_hash, t.mrkl_hash, t.timestamp, t.difficulty, t.txs, nonce, hash);
+	setTransactions (txs) {
+		if (txs.length !== this.txsLength)					return false;
+		if (JSON.stringify(txs).length !== this.txsSize)	return false;
+		if (!Block.isMrklHashValid(this.mrkl_hash, txs))	return false;
+		this.txs = txs;
+		return true;
+	}
+
+	toString (include_txs=false) {
+		return JSON.stringify(Block.encode(this,include_txs));
+	}
+};
+	Block.version = 1;
+	/* encode & decode */
+		Block.encode = function (block,include_txs=false) {//Object => Array
+			let result = [block.hash, block.index, block.version, block.prev_hash, block.mrkl_hash, block.timestamp, block.difficulty, block.txsLength, block.txsSize, block.nonce];
+			if (include_txs)
+				result.push(block.txs);
+			return result;
+		};
+		Block.decode = function (block) {//(Array | String) => Object
+			if (block+"" === block)
+				block = JSON.parse(block);
+			return new Block(...block);
+		};
+
+	/* hash functions */
+		Block.calcBlockHash = function (block) {//block = Object
+			let blockdata = Block.encode(block);
+			blockdata.shift();
+			return util.sha256( JSON.stringify(blockdata) );
+		};
+
+		Block.calcMrklHash = function (txs) {
+			if (txs.length % 2 !== 0)	throw `calcMrklHash : txs.length is not even`;
+
+			let hashes = txs.map(tx => util.sha256(tx));
+			while (hashes.length > 1) {
+				let nextHashBuffer = [];
+				for (let i=0; i<hashes.length; i+=2)
+					nextHashBuffer.push( util.sha256(hashes[i]+hashes[i+1]) );
+				hashes = nextHashBuffer;
+			}
+			return hashes[0];
+		};
+
+	/* check functions */
+		Block.isBlockValid = function (block) {
+			return Block.isBlockHeadValid(block)
+				&& Block.isMrklHashValid(block.mrkl_hash, block.txs);
+		};
+
+		Block.isBlockHeadValid = function (block) {
+			return Block.isBlockDifficultyValid(block.hash, block.difficulty)
+				&& block.hash === Block.calcBlockHash(block);
+		};
+
+		Block.isBlockDifficultyValid = function (hash, difficulty) {
+			return (hash+"").substr(0, difficulty) === util.toHex(0,difficulty)
+		};
+
+		Block.isMrklHashValid = function(mrkl_hash, txs) {
+			return mrkl_hash === Block.calcMrklHash(txs);
+		};
+
+
+	Block.MineHelper = class BlockMineHelper {
+
+		constructor (index, version, prev_hash, difficulty, txs=[]) {
+
+			this.hash = "";
+
+			this.index = index;
+			this.version = version;
+
+			this.prev_hash = prev_hash;
+			this.mrkl_hash = Block.calcMrklHash(txs);
+
+			this.timestamp = Date.now();
+			this.difficulty = difficulty;
+
+			this.txsLength = txs.length;
+			this.txsSize = JSON.stringify(txs).length;
+
+			this.nonce = 0;
+			this.txs = txs;
+		}
+
+		mine () {
+			this.hash = Block.calcBlockHash(this);
+
+			let t = this;
+			if (Block.isBlockDifficultyValid(this.hash, this.difficulty)) {
+				return new Block(t.hash, t.index, t.version, t.prev_hash, t.mrkl_hash, t.timestamp, t.difficulty, t.txsLength, t.txsSize, t.nonce, t.txs);
+			}
 			return false;
 		}
 	};
