94477d96b743b5995e1ee9640a72b8e4a3936fad
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-17 19:52:20 +0900

    simple Peer update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const util = require('./util.js');
 
+const Block = require('./block.js');
 const BlockChain = require('./blockchain.js');
 
 const WS = require('ws');
@@ -9,6 +10,7 @@ const WS = require('ws');
 		address:null,
 		server:null,
 		sockets:{},
+		firstOpen:true,
 
 		listen () {
 			this.address = `ws://${getIp()}:${this.port}`;
@@ -28,12 +30,19 @@ const WS = require('ws');
 		},
 
 		connectTo (addr) {
+			if (this.hasConnectionTo(addr) || this.address === addr)	return false;
+
 			const client = new WS(addr);
 			this.addSocket(addr, client, true);
 
 			client.on('open', e => {
 				client.send(this.address);
-				Protocol.messager(client, 'chain-request');
+
+				if (SocketServer.firstOpen) {
+					Protocol.messager(client, 'addrs-request');
+					Protocol.messager(client, 'chain-request');	
+					SocketServer.firstOpen = false;
+				}
 			});
 			client.on('message', Protocol.handler.bind(Protocol, client));
 			client.on('close', e => this.removeSocket(addr, client, true));
@@ -56,6 +65,7 @@ const WS = require('ws');
 		},
 
 		broadCast (message, socket=false) {
+			if (typeof message !== "string")	message = JSON.stringify(message)
 			for (let addr in this.sockets) {
 				if (this.sockets[addr].socket)		this.sockets[addr].socket.send(message);
 				else if(this.sockets[addr].client)	this.sockets[addr].client.send(message);
@@ -65,26 +75,67 @@ const WS = require('ws');
 		addrs () {
 			return Object.keys(this.sockets);
 		},
+		hasConnectionTo (addr) {
+			return this.sockets[addr] && ( this.sockets[addr].client || this.sockets[addr].socket );
+		},
 	};
 
 	const Protocol = {
+		'addrs-request':{
+			messager () { return {}; },
+			handler (socket, msg) { Protocol.messager(socket, 'addrs-response'); }
+		},
+		'addrs-response':{
+			messager () { return {addrs:SocketServer.addrs()}; },
+			handler (socket, msg) { msg.addrs.forEach(addr => SocketServer.connectTo(addr)); }
+		},
+
 		'chain-request':{
 			messager () { return {}; },
 			handler (socket, msg) { Protocol.messager(socket, 'chain-response'); }
 		},
 		'chain-response':{
-			messager () { return {blocks:BlockChain.blocks().map(block => block+"")} },
-			handler (socket, msg) { console.log(msg); }
+			messager () { return {blocks:BlockChain.blocks().map(block => block+"")}; },
+			handler (socket, msg) {
+				if (BlockChain.newChain(msg.blocks.map(v => Block.decode(v)))) {
+					console.log("accept");
+				} else
+					console.log("not accept");
+			}
+		},
+		'chain-broadcasting':{
+			messager () { return {blocks:BlockChain.blocks().map(block => block+"")}; },
+			handler (socket, msg) {
+				if (BlockChain.newChain(msg.blocks.map(v => Block.decode(v)))) {
+					console.log("accept");
+					SocketServer.broadCast(msg, socket);
+				} else
+					console.log("not accept");
+			}
 		},
 
-		messager (socket, type,...args) {
+		messager (socket, type, ...args) {
 			if (!Protocol[type])	return "";
 
 			let msg = Protocol[type].messager(...args);
 			msg.timestamp = Date.now();
 			msg.type = type;
 
+			console.log("send", msg);
+
 			socket.send( JSON.stringify(msg) );
+		},
+
+		broadCaster (type, ...args) {
+			if (!Protocol[type])	return "";
+
+			let msg = Protocol[type].messager(...args);
+			msg.timestamp = Date.now();
+			msg.type = type;
+
+			console.log("broadCast", msg);
+
+			SocketServer.broadCast( JSON.stringify(msg) );
 		},
 
 		handler (socket, msg_string) {
@@ -92,11 +143,16 @@ const WS = require('ws');
 				let msg = JSON.parse(msg_string);
 				if (!Protocol[msg.type])	return false;
 
+				console.log("recive", msg);
 				Protocol[msg.type].handler(socket, msg);
 			} catch (e) {
-				//throw e;
+				console.error(e);
 			}
 		},
+	};
+
+	BlockChain.onOnMine = function () {
+		Protocol.broadCaster('chain-broadcasting', BlockChain.blocks().map(v => v+""));
 	};
 
 const express = require('express');
@@ -116,10 +172,9 @@ const express = require('express');
 				app.get('/blocks', (req, res) => res.send(BlockChain.blocks()));
 
 				app.get('/mine', (req, res) => {
-					let mineData = BlockChain.mine();
-					res.send(mineData);
+					BlockChain.updateMiner();
+					res.send("mining");
 				});
-
 
 			/* webSocket && peers */
 				app.get('/server', function (req, res) {
@@ -133,17 +188,19 @@ const express = require('express');
 					res.send ( SocketServer.addrs() );
 				});
 
-			app.listen(this.port,'localhost');
+			app.listen(this.port);
 			console.log('listen on : http://localhost:'+this.port);
 		}
 	};
 
 SocketServer.listen();
-HttpServer.listen();
+//HttpServer.listen();
 
 if ( process.argv[2] ) {
 	SocketServer.connectTo( `ws://localhost:`+process.argv[2] );
 }
+
+BlockChain.updateMiner();
 
 
 function getIps() {
@@ -160,3 +217,7 @@ function getIps() {
 };
 
 function getIp() { return getIps()[0]; };
+
+process.on('uncaughtException', function (err) {
+    console.error(err);
+}); 
