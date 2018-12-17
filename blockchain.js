3be757e2865bbd18d0c9dbc278bfcd441191613e
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-17 16:53:55 +0900

    add transaction compute & update mining code

--- blockchain.js
+++ blockchain.js
@@ -5,6 +5,7 @@ const Mine = require('./mine.js');
 const util = require('./util.js');
 
 	const BlockChain = {
+		transactions:[],
 		chain:new Chain(),
 		wallet:new Wallet(),
 
@@ -12,20 +13,39 @@ const util = require('./util.js');
 		blocks () { return this.chain.blocks },
 		block (i) { return this.chain.blocks[i] },
 
-		mine () {
-			let transaction = new Transaction.Builder.Transmission(util.toHex(0,64), this.wallet.getAddress(), 100).sign(this.wallet)+"";
+		addTransactions (transactions) {
+			for (let transaction of transactions) {
+				transaction = transaction+"";
 
-			let block;
+				if (!Transaction.verify(transaction))				continue;
+				if (this.transactions.indexOf(transaction) >= 0)	continue;
 
-			if (this.chain.blocks.length == 0)
-				block = Mine.mineGenesis([transaction,"padding"]);
-			else
-				block = Mine.mineWithBlock(this.chain.topBlock, [transaction,"padding"]);
-
-			return {
-				isAdded:this.chain.newChain([...this.chain.blocks,block]),
-				block
+				this.transactions.push(transaction);
 			}
+		},
+
+		removeTransactions (transactions) {
+			this.transactions = this.transactions.filter(transaction => transactions.indexOf(transaction) < 0);
+		},
+
+		newChain (chain) {
+			let data = this.chain.newChain(chain);
+
+			if (data) {
+				this.addTransactions(data.removedTransactions);
+				this.removeTransactions(data.addedTransactions);
+			}
+		},
+
+		updateMiner () {
+			let transaction = new Transaction.Builder.Transmission(util.toHex(0,64), this.wallet.getAddress(), 100).sign(this.wallet);
+			if (this.chain.length == 0)	return Mine.mineGenesis([transaction, ...this.transactions]);
+			else						return Mine.mineNextBlock(this.chain.topBlock, [transaction, ...this.transactions]);
+		},
+		
+		onMine (block) {
+			this.newChain([block]);
+			this.updateMiner();
 		},
 
 		walletInfo (private=false) {
@@ -42,9 +62,11 @@ const util = require('./util.js');
 				public:this.wallet.getPublicKey()
 			};
 		},
+
 	};
 
 	Mine.wallet = BlockChain.wallet;
+	Mine.onMine = BlockChain.onMine.bind(BlockChain);
 
 module.exports = BlockChain;
 module.exports.version = 1;
