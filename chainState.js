aee03c761099c4f708f98e9222790fabb51ea013
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-18 23:48:34 +0900

    Update chainState.js

--- chainState.js
+++ chainState.js
@@ -1,3 +1,5 @@
+const util = require('./util.js');
+const Wallet = require('./wallet.js');
 const Transaction = require('./transaction.js');
 
 class ChainState {
@@ -15,41 +17,78 @@ class ChainState {
 
 		transaction = Transaction.decode(transaction);
 
-		ChainState.update(this.state, JSON.parse(transaction.data));
-
-		console.log(this.state);
+		ChainState.update(this.state, JSON.parse(transaction.data), transaction);
 	}
 
+	/**
+	 * @param {string[]} transactions
+	 */
 	addTransactions (transactions) {
 		for (let transaction of transactions)
 			this.addTransaction(transaction);
 	}
 
+	/**
+	 * @param {object} block
+	 */
 	addBlock (block) {
+		this.state.block = block;
+		this.state.index = block.index;
 		this.addTransactions(block.txs);
 	}
 
+	/**
+	 * @param {object[]} blocks
+	 */
 	addBlocks (blocks) {
-		for (let block of blocks)
-			this.addTransactions(block.txs);
+		for (let block of blocks) 
+			this.addBlock(block);
 	}
 };
-	ChainState.update = function (state, data) {
+	/**
+	 * calculate state from data
+	 *
+	 * @param {object} state : state to update
+	 * @param {object} data : transaction's data
+	 * @param {object} transaction
+	 */
+	ChainState.update = function (state, data, transaction) {
 		if (ChainState.handlers[data.type])
-			ChainState.handlers[data.type](state, data);
+			ChainState.handlers[data.type](state, data, transaction);
 	};
 		ChainState.handlers = {
-			'create-address' (store, addr) {
-				if (!store[addr])	store[addr] = {amount:0};
+			'create-address' (state, data, transaction) {
+				if (!state[data.addr])	state[data.addr] = {amount:0};
 			},
 
-			transmission (store, data) {
-				this['create-address'](store, data.receiveAddr);
-				this['create-address'](store, data.sendAddr);
+			transmission (state, data, transaction) {
+				this['create-address'](state, {addr:data.receiveAddr});
+				this['create-address'](state, {addr:data.sendAddr});
 
-				store[data.receiveAddr].amount += data.amount;
-				store[data.sendAddr].amount -= data.amount;
-			}
+				if (data.sendAddr === util.zeros64) {
+					if (Wallet.getAddressFromPublicKey(transaction.publicKey) !== data.receiveAddr)	return false;
+				} else if (Wallet.getAddressFromPublicKey(transaction.publicKey) !== data.sendAddr)	return false;
+
+				state[data.receiveAddr].amount += data.amount;
+				state[data.sendAddr].amount -= data.amount;
+			},
+
+			'get-miner-permission' (state, dataObject, transaction) {
+				this['create-address'](state, {addr:transaction.address});
+
+				let sign = transaction.sign,
+					data = Transaction.encode(transaction,true),
+					publicKey = Wallet.publicKey2Pem(state.block.publicKey);
+
+				if (!Wallet.verifySign(data, sign, publicKey))	return false;
+
+				state[transaction.addr].miner = {
+					when:state.index,
+					blockHash:block.hash,
+					transactionHash:transaction.hash,
+					name:util.sha256(state.block.hash + transaction.hash),
+				};
+			},
 		};
 
 module.exports = ChainState;
