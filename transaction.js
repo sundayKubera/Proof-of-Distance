906a30ec787eb62584db300c56509e763f351db2
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-18 12:10:15 +0900

    add comment

--- transaction.js
+++ transaction.js
@@ -2,6 +2,16 @@ const Wallet = require('./wallet.js');
 const util = require('./util.js');
 
 class Transaction {
+	/**
+	 * Create Basic Transaction Object
+	 *
+	 * @param {string} hash : hash of (address, publicKey, data, timestamp)
+	 * @param {string} sign : wallet.getSign(hash)
+	 * @param {string} address : wallet.getAddress()
+	 * @param {string} publicKey : wallet.getPublicKey()
+	 * @param {any} data : some data
+	 * @param {int} timestamp : Generated time
+	 */
 	constructor (hash, sign, address, publicKey, data, timestamp) {
 		this.hash = hash;
 		this.sign = sign;
@@ -11,16 +21,35 @@ class Transaction {
 		this.timestamp = timestamp;
 	}
 
+	/**
+	 * Convert Transaction Object into String
+	 *  is Transaction.encode really need?
+	 *
+	 * @return {string}
+	 */
 	toString () {
 		return JSON.stringify(Transaction.encode(this,true));
 	}
 };
 	/* encode & decode */
-		Transaction.encode = function (transaction,include_hash=false) {//Object => Array
-			if (include_hash)
+		/**
+		 * Convert Transaction Object into Array
+		 *
+		 * @param {object} transaction
+		 * @param {boolen} isNotForHash : default true
+		 * @return {string}
+		 */
+		Transaction.encode = function (transaction,isNotForHash=false) {//Object => Array
+			if (isNotForHash)
 				return [transaction.hash, transaction.sign, transaction.address, transaction.publicKey, transaction.data, transaction.timestamp];
 			return [transaction.address, transaction.publicKey, transaction.data, transaction.timestamp];
 		};
+		/**
+		 * Convert Array into Transaction Object
+		 *
+		 * @param {string} transaction : Transaction.encode(...)
+		 * @return {object} : instanceof Transaction
+		 */
 		Transaction.decode = function (transaction) {//(Array | String) => Object
 			if (transaction+"" === transaction)
 				transaction = JSON.parse(transaction);
@@ -28,14 +57,20 @@ class Transaction {
 		};
 
 	/* check */
+		/**
+		 * Check if Transaction Valid
+		 *  check hash & sign & address
+		 *
+		 * @param {object|array|string} transaction
+		 * @return {boolen}
+		 */
 		Transaction.verify = function (transaction) {
 			let tx = transaction;
 
-			if (tx+"" === tx)
-				tx = Transaction.decode(JSON.parse(transaction));
+			if (tx+"" === tx)			tx = JSON.parse(tx);
+			if (tx instanceof Array)	tx = Transaction.decode(tx);
 
-			if (util.sha256(JSON.stringify(Transaction.encode(tx, false))) !== tx.hash)
-																							return false;	//it attacked by someone!!!
+			if (util.sha256(JSON.stringify(Transaction.encode(tx, false))) !== tx.hash)		return false;	//it attacked by someone!!!
 			if (!Wallet.verifySign(tx.hash, tx.sign, Wallet.publicKey2Pem(tx.publicKey)))	return false;	//it is fake!!!
 			if (Wallet.getAddressFromPublicKey(tx.publicKey) !== tx.address)				return false;	//it is fake!!!
 
@@ -43,14 +78,23 @@ class Transaction {
 		};
 
 class TransactionBuilder {
+	/**
+	 * Create Basic Transaction Builder Object
+	 *
+	 */
 	constructor () {
 		this.data = {};
 	}
 
+	/**
+	 * Sign on this Transaction
+	 *
+	 * @param {object} wallet : needs to sign
+	 * @return {object} : Transaction Object
+	 */
 	sign (wallet) {
 		let data_json = JSON.stringify(this.data);
 		let transaction_json = Transaction.encode({
-			//sign:		wallet.getSign(data_json),
 			address:	wallet.getAddress(),
 			publicKey:	wallet.getPublicKey(),
 			data:		data_json,
@@ -67,6 +111,15 @@ class TransactionBuilder {
 
 
 class TransmissionBuilder extends TransactionBuilder {
+	/**
+	 * Create Basic TransmissonTransaction Builder Object
+	 *  it needs to sign by sendAddr
+	 *  but it can sign by reciveAddr when sendAddr is "0"*64
+	 *
+	 * @param {string} sendAddr
+	 * @param {string} receiveAddr
+	 * @param {int} amount
+	 */
 	constructor (sendAddr, 	receiveAddr, amount) {
 		super();
 		this.data.type = "transmission";
