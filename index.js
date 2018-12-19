041d6545ab2da5f147429db40afa889f628ab76d
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-19 22:55:26 +0900

    basic structure

---
+++ index.js
@@ -0,0 +1,4 @@
+const Storage = new (require('./storage.js'))();
+const Bus = new (require('./bus.js'))();
+
+console.log(Storage, Bus);
---
+++ storage.js
@@ -0,0 +1,5 @@
+class Stroage {
+
+};
+
+module.exports = Stroage;
