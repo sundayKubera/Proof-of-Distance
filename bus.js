59cd290cc4e3f0c2e747b74a4ab7883bf4f891c4
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-19 23:32:44 +0900

    add off & comment

--- bus.js
+++ bus.js
@@ -5,20 +5,44 @@ class Bus extends EventEmitter {
 		super();
 	}
 
+
+	/**
+	 * emit that needs Response
+	 *
+	 * @return {object} : promise
+	 */
 	request (name, ...args) {
 		return new Promise((resolve, reject) => {
 			this.emit(name, {resolve, reject}, ...args);
 		});
 	}
 
+	/**
+	 * on that makes Response
+	 *
+	 * @param {string|sybole} name
+	 * @param {function} listener
+	 * @return {object} : self
+	 */
 	onRequest (name, listener) {
-		return this.on(name, (response, ...args) => {
+		return this.on(name, listener.listener = (response, ...args) => {
 			try {
 				response.resolve(listener(...args));
 			} catch (error) {
 				response.reject(error);
 			}
 		});
+	}
+
+	/**
+	 * off that makes Response
+	 *
+	 * @param {string|sybole} name
+	 * @param {function} listener
+	 * @return {object} : self
+	 */
+	offRequest (name, listener) {
+		return this.off(name, listener.listener);
 	}
 }
 
