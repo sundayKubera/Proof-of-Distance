365dee9da1647993b58bd7163c73e531754d405e
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-19 23:23:47 +0900

    add basic methods

--- bus.js
+++ bus.js
@@ -4,6 +4,23 @@ class Bus extends EventEmitter {
 	constructor () {
 		super();
 	}
+
+	request (name, ...args) {
+		return new Promise((resolve, reject) => {
+			this.emit(name, {resolve, reject}, ...args);
+		});
+	}
+
+	onRequest (name, listener) {
+		return this.on(name, (response, ...args) => {
+			try {
+				response.resolve(listener(...args));
+			} catch (error) {
+				response.reject(error);
+			}
+		});
+	}
 }
 
 module.exports = Bus;
+module.exports.version = 1;
