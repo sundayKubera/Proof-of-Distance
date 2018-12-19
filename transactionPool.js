33ca99c71ba37ef31b07ec96238f32e92f74bde4
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-19 22:33:11 +0900

    add return boolean

--- transactionPool.js
+++ transactionPool.js
@@ -13,25 +13,30 @@ class TransactionPool {
 	 * add Transactions to Transaction Pool
 	 * 
 	 * @param {string[]} transactions
+	 * @result {boolean}
 	 */
 	addTransactions (transactions) {
+		let beforeSize = this.pool.length;
 		for (let transaction of transactions) {
 			if (transaction === "padding")				continue;
 			if (this.pool.indexOf(transaction) >= 0)	continue;
-			if (!Transaction.verify(transaction))		return false;
+			if (!Transaction.verify(transaction))		continue;
 
 			this.pool.push(transaction);
 		}
-		return true;
+		return beforeSize !== this.pool.length
 	}
 
 	/**
 	 * remove Transactions from Transaction Pool
 	 * 
 	 * @param {string[]} transactions
+	 * @result {boolean}
 	 */
 	removeTransactions (transactions) {
+		let beforeSize = this.pool.length;
 		this.pool = this.pool.filter(transaction => transactions.indexOf(transaction) < 0);
+		return beforeSize !== this.pool.length
 	}
 };
 
