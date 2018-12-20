c960c52fc2b1345d2e4b3bd2a8d5dd53ab334d00
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-20 18:45:40 +0900

    console.log remove linebreak

--- multiprocess.js
+++ multiprocess.js
@@ -3,8 +3,8 @@ const { spawn } = require('child_process');
 function spawnIndex (i) {
 	const child = spawn('node', i==0 ? ['index.js'] : ['index.js',8000]);	
 
-	child.stdout.on('data', (data) => console.log(`${i}: ${data}`));
-	child.stderr.on('data', (data) => console.error(`${i}: ${data}`));
+	child.stdout.on('data', (data) => console.log(`${i}: ${(data+"").substr(0, data.length-1)}`));
+	child.stderr.on('data', (data) => console.error(`${i}: ${(data+"").substr(0, data.length-1)}`));
 	child.on('close', (code) => console.log(`${i} exited with code ${code}`));
 }
 
