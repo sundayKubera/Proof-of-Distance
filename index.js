eca52db45d7d1f74ede699446dc49d3232cb4b5e
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-20 12:22:29 +0900

    add socketServer.js

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const Storage = new (require('./storage.js'))();
 const Bus = new (require('./bus.js'))();
 
+let ip = (() => {
+	let ifaces = require('os').networkInterfaces(), ips = [];
+
+	for (let ifname in ifaces) {
+		for (let iface of ifaces[ifname]) {
+			if ('IPv4' !== iface.family || iface.internal !== false)	continue;
+			ips.push(iface.address);
+		}
+	}
+
+	return ips;
+})()[0];
+
+Storage.set('ENV.SocketServer.host', ip);
+
 if ( process.argv[2] ) {
-	Storage.set('ENV.SocketServer.peers', [`ws://localhost:`+process.argv[2]]);
+	Storage.set('ENV.SocketServer.seedPeers', [`ws://localhost:`+process.argv[2]]);
 	Storage.set('ENV.SocketServer.port', 8002+Math.floor(Math.random()*1000));
 	Storage.set('ENV.HttpServer.port', 8002+Math.floor(Math.random()*1000));
 } else {
-	Storage.set('ENV.SocketServer.peers', []);
+	Storage.set('ENV.SocketServer.seedPeers', []);
 	Storage.set('ENV.SocketServer.port', 8000);
 	Storage.set('ENV.HttpServer.port', 8001);
 }
 
-let sctipts = [];
+let sctipts = ['./socketServer.js'];
 for (let script of sctipts)
 	require(script)(Storage,Bus);
 
