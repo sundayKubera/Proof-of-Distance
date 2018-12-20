7d8ff0e190a23a7b9892aa7571ee37712843be25
sundayKubera <45848689+sundayKubera@users.noreply.github.com>
2018-12-20 19:25:02 +0900

    fix error

--- mine.js
+++ mine.js
@@ -1,4 +1,12 @@
 module.exports = (Storage,Bus) => {
+	const Mine = {
+		miningLoop () {
 
+		},
+	};
+
+	Bus.on('init', () => {
+		setInterval(Mine.miningLoop.bind(Mine,10));
+	});
 };
 module.exports.version = 2;
